3f3f184fbf12655ab2a88afd03314da14a3889a5
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2020-10-26 09:09:32 -0300

    Inicializada styled-components

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Github</Title>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-use-before-define */
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
