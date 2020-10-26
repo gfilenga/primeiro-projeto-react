f4bb42660280830cd2cfe4305e55848424085783
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2020-10-26 10:17:19 -0300

    Estilizando o Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,60 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoImage from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/67002731?s=460&u=6f5d7e06045169437dea4e0ccd11aa478098ff1b&v=4"
+            alt="Guilherme Filenga"
+          />
+          <div>
+            <strong>primeiro-projeto-react</strong>
+            <p>ReactJS + Typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/67002731?s=460&u=6f5d7e06045169437dea4e0ccd11aa478098ff1b&v=4"
+            alt="Guilherme Filenga"
+          />
+          <div>
+            <strong>primeiro-projeto-react</strong>
+            <p>ReactJS + Typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/67002731?s=460&u=6f5d7e06045169437dea4e0ccd11aa478098ff1b&v=4"
+            alt="Guilherme Filenga"
+          />
+          <div>
+            <strong>primeiro-projeto-react</strong>
+            <p>ReactJS + Typescript</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
